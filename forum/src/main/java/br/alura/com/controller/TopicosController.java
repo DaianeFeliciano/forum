93898fb2c1395dec5127a3896d5bf9faef43fb1b
Daiane Feliciano <77757301+DaianeFeliciano@users.noreply.github.com>
2021-07-25 13:51:41 -0300

    alteração na classe Dto

--- forum/src/main/java/br/alura/com/controller/TopicosController.java
+++ forum/src/main/java/br/alura/com/controller/TopicosController.java
@@ -4,9 +4,9 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.alura.com.controller.dto.TopicoDto;
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
 
@@ -14,13 +14,13 @@ import br.com.alura.forum.modelo.Topico;
 public class TopicosController {
 	
 	@RequestMapping("/topicos") // endereço quando digita esse endereço, chama esse método
-	public List<Topico>lista() {
+	public List<TopicoDto>lista() {
 		
 	// retornar a lista	
 		Topico topico = new Topico("Dúvida", "Duvida com Spring", new Curso("Spring", "Programação"));
 		
-		return Arrays.asList(topico, topico, topico);
-		// criando objeto
+		return TopicoDto.converter(Arrays.asList(topico, topico, topico));
+		// criando objeto / converter de topico para topico dto
 		
 	}
 
--- forum/src/main/java/br/alura/com/controller/dto/TopicoDto.java
+++ forum/src/main/java/br/alura/com/controller/dto/TopicoDto.java
@@ -1,26 +1,30 @@
 package br.alura.com.controller.dto;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import br.com.alura.forum.modelo.Topico;
 
 public class TopicoDto {
+	// atributos campos do topicos que quero devolver
 	
 	private Long id;
 	private String titulo;
 	private String mensagem;
 	private LocalDateTime dataCriacao;
 	
+	// toda vez que der new no topicodto passa como parametro um objeto do tipo tópico
 	public TopicoDto(Topico topico) {
-		
-		this.id = topico.getId();
+		// onde vai puxar os dados = construtor
+		this.id = topico.getId(); // recebo o topido e preencho os atributos
 		this.titulo = topico.getTitulo();
 		this.mensagem = topico.getMensagem();
 		this.dataCriacao = topico.getDataCriacao();
 		
 		
 	}
-	
+	// gets só gerar os gets nessa classe
 	public Long getId() {
 		return id;
 	}
@@ -33,6 +37,12 @@ public class TopicoDto {
 	public LocalDateTime getDataCriacao() {
 		return dataCriacao;
 	}
+	
+	public static List<TopicoDto> converter(List<Topico> topicos) {
+	    return topicos.stream().map(TopicoDto::new).collect(Collectors.toList());
+	        } // esse metodo recebe o uma lista de topicos e precisa devolver uma lista de topicos dto
+				// conversao mapear topico para topico dto // chamando construtor // collectors
+				// transforma numa lista
 	
 
 }
