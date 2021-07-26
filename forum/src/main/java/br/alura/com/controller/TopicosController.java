371adfb97665c16d9d2310f637f5c18900c5af3d
Daiane Feliciano <77757301+DaianeFeliciano@users.noreply.github.com>
2021-07-25 21:11:22 -0300

    inclusão de atualizar

--- forum/src/main/java/br/alura/com/controller/TopicosController.java
+++ forum/src/main/java/br/alura/com/controller/TopicosController.java
@@ -3,18 +3,23 @@ package br.alura.com.controller;
 import java.net.URI;
 import java.util.List;
 
+import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import br.alura.com.controller.dto.DatalhesDto;
 import br.alura.com.controller.dto.TopicoDto;
+import br.alura.com.controller.form.AtualizacaoTopicoForm;
 import br.alura.com.controller.form.TopicoForm;
 import br.alura.com.forum.modelo.Topico;
 import br.alura.com.repository.CursoRepository;
@@ -56,9 +61,26 @@ public class TopicosController {
 	        URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
 	        return ResponseEntity.created(uri).body(new TopicoDto(topico));
 	        
-	        
-	
+	   
 	}
+	
+	@GetMapping("/{id}")
+	public DatalhesDto detalhar(@PathVariable Long id) {
+		
+		Topico topico = topicoRepository.getById(id);
+		return new DatalhesDto(topico);
+		
+	}
+	
+	@PutMapping("/{id}")
+	@Transactional
+	public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form) {
+		
+		Topico topico = form.atualizar(id, topicoRepository);
+		
+		return ResponseEntity.ok(new TopicoDto(topico));
+	}
+	
 	
 }
 
---
+++ forum/src/main/java/br/alura/com/controller/dto/DatalhesDto.java
@@ -0,0 +1,63 @@
+package br.alura.com.controller.dto;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import br.alura.com.forum.modelo.StatusTopico;
+import br.alura.com.forum.modelo.Topico;
+
+public class DatalhesDto {
+	
+	private Long id;
+	private String titulo;
+	private String mensagem;
+	private LocalDateTime dataCriacao;
+	private String nomeAutor;
+	private StatusTopico status;
+	private List<RespostaDto> respostas;
+	
+	public DatalhesDto (Topico topico) {
+		// onde vai puxar os dados = construtor
+		this.id = topico.getId(); // recebo o topido e preencho os atributos
+		this.titulo = topico.getTitulo();
+		this.mensagem = topico.getMensagem();
+		this.dataCriacao = topico.getDataCriacao();
+		this.nomeAutor = topico.getAutor().getNome();
+		this.status = topico.getStatus();
+		this.respostas = new ArrayList<>();
+		this.respostas.addAll(topico.getRespostas().stream().map(RespostaDto::new).collect(Collectors.toList()));
+		
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public String getMensagem() {
+		return mensagem;
+	}
+
+	public LocalDateTime getDataCriacao() {
+		return dataCriacao;
+	}
+
+	public String getNomeAutor() {
+		return nomeAutor;
+	}
+
+	public StatusTopico getStatus() {
+		return status;
+	}
+
+	public List<RespostaDto> getRespostas() {
+		return respostas;
+	}
+
+}
